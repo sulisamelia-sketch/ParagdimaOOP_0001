20b28cbf7c9d53c47a945f7e236cc69e8b0571b9
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-20 10:26:23 +0700

    first commit

---
+++ ParagdimaOOP.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+
+public :
+    int nim;
+    string nama;
+    float nilai;
+
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "NAMA : " << nama << endl;
+        cout << "NILAI : " << nilai << endl;
+    }
+
+}; // batas akhir kelas
+
+int main(){
+    Mahasiswa mhs1;
+    mhs1.nim = 0001;
+    mhs1.nama = "Sulis Amelia";
+    mhs1.nilai = 90;
+
+    mhs1.printData ();
+}
